86e92aadddf258e25310c44f2c357f5e98a31567
brandatta <87575253+brandatta@users.noreply.github.com>
2025-11-14 11:48:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ st.markdown("""
 if logo_b64:
     st.markdown(f"""
     <div class="header-container">
-        <div class="header-title">Subida de CSV/XLSX → <strong>corven.crudo_ap</strong></div>
+        <div class="header-title">Input FBL1N</div>
         <div class="header-logo"><img src="data:image/png;base64,{logo_b64}" /></div>
     </div>
     """, unsafe_allow_html=True)
